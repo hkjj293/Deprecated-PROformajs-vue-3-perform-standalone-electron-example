cb28126ac3501e49d455c054ca9ff4e84cd12734
Matt South <matt.south@openclinical.net>
2023-04-16 13:40:31 +0100

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import our custom CSS
+import './assets/styles.scss'
+
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    keepNames: true,
+    minifyIdentifiers: false
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      minifyIdentifiers: false,
+      keepNames: true,
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': resolve(__dirname , 'node_modules/bootstrap')
     }
   }
 })
